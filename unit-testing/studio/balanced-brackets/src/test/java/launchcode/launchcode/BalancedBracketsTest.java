9800f787501a37bffbe7431746041605fe23fd4b
Jordan <138933858+Roebot926@users.noreply.github.com>
2023-08-31 11:27:01 -0400

    studio

--- unit-testing/studio/balanced-brackets/src/test/java/launchcode/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/launchcode/launchcode/BalancedBracketsTest.java
@@ -5,9 +5,67 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
-    //TODO: add tests here
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void nonBracket() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void launchBracket() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void rightCodeBracket() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void fullBracketLeft() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void missingRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void bracketsOppositeRandom(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void commaSpace(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(", "));
+    }
+
+    @Test
+    public void backwardsBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void paramValidationMaybe(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void whoKnows(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]Launch[[Code]]"));
+    }
+
+
+
+
 }
